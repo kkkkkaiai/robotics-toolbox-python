c0d7adec5c8f6981163fedc3074951612f1d0d7b
KaiChen <597264831@qq.com>
2022-02-22 22:31:04 +0800

    添加车臂模型

--- roboticstoolbox/scripts/car_arm.py
+++ roboticstoolbox/scripts/car_arm.py
@@ -13,21 +13,18 @@ from roboticstoolbox import ERobot
 import tempfile as tf
 from roboticstoolbox.tools.data import rtb_path_to_datafile
 
-env = swift.Swift()
-env.launch(realtime=True)
 
-
-class mr500(ERobot):
+class mr500_kinova(ERobot):
     def __init__(self):
         links, name, urdf_string, urdf_filepath = self.URDF_read(
-            "mr500_description/urdf/mr500.urdf.xacro",
+            "mr500_kinova_description/urdf/mr500_kinova.urdf.xacro",
         )
 
         super().__init__(
             links,
             name=name,
-            manufacturer="shihe",
-            gripper_links=links[5],
+            manufacturer="mine",
+            gripper_links=links[14],
             urdf_string=urdf_string,
             urdf_filepath=urdf_filepath,
         )
@@ -37,12 +34,20 @@ class mr500(ERobot):
                 1.6,
                 1.6,
                 1.6,
-                1.6
+                1.6,
+                2.1750,
+                2.1750,
+                2.1750,
+                2.6100,
+                2.6100,
+                2.6100
             ]
         )
 
-        self.addconfiguration("qz", np.array([0, 0, 0, 0]))
-        self.addconfiguration("qr", np.array([0, 0, 0, 0]))
+        print(self.n)
+
+        self.addconfiguration("qz", np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]))
+        self.addconfiguration("qr", np.array([0, 0, 0, 0,  -0.3, 0, -1.6, 0, -1.0, np.pi / 2]))
 
         for i in range(self.n):
             if np.any(self.qlim[:, i] != 0) and not np.any(np.isnan(self.qlim[:, i])):
@@ -50,24 +55,26 @@ class mr500(ERobot):
         print(self._valid_qlim)
 
 
-mr500_dir = "./mr500_description"
-if not path.exists(mr500_dir):
-    print(mr500_dir, ' is not exist')
+mr500_kinova_dir = "./mr500_kinova_description"
+if not path.exists(mr500_kinova_dir):
+    print(mr500_kinova_dir, ' is not exist')
     sys.exit()
 
 xacro_path = rtb_path_to_datafile("xacro")
-mr500_xacro = xacro_path / "mr500_description"
+mr500_kinova_xacro = xacro_path / "mr500_kinova_description"
 
-print('source dir: ', mr500_xacro, '\ndst dir: ', mr500_dir)
-copy_tree(mr500_dir, str(mr500_xacro))
+print('source dir: ', mr500_kinova_xacro, '\ndst dir: ', mr500_kinova_dir)
+copy_tree(mr500_kinova_dir, str(mr500_kinova_xacro))
 
 dt = 0.025
 
-car = mr500()
-car.q = car.qr
-print(car.q)
-env.add(car)
+env = swift.Swift()
+env.launch(realtime=True)
+
+car_m = mr500_kinova()
+car_m.q = car_m.qr
+print(car_m.q)
+env.add(car_m)
 env.step(dt)
 
 env.hold()
-
